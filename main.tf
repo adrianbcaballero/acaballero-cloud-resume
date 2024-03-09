8d3246b96533f8d5a35665803c86c056ac189f5a
adrianbcaballero <abcaballero.py@gmail.com>
2024-03-08 17:52:31 -0800

    front end creation and ci

--- main.tf
+++ main.tf
@@ -2,5 +2,17 @@ provider "aws" {
   region = "us-west-1"
 }
 
-//test for plan action
-resource "random_pet" "server" {}
+//creating s3 bucket for branch frontend content"
+resource "aws_s3_bucket" "adriancaballero-branchcontent" {
+  bucket = "adriancaballero-branchcontent"
+  region = "us-west-1"
+}
+
+resource "aws_s3_bucket_public_access_block" "adriancaballero-branchcontent" {
+  bucket = aws_s3_bucket.adriancaballero-branchcontent.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
