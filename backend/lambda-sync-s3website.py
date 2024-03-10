67887151fd677c6a85484b7e6ef7c79e17415c8f
adrianbcaballero <abcaballero.py@gmail.com>
2024-03-10 04:19:41 -0700

    updated lambda to properly sync s3

--- backend/lambda-sync-s3website.py
+++ backend/lambda-sync-s3website.py
@@ -9,23 +9,24 @@ sns_topic_arn = "${sns_topic_arn}"
 
 def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = event['Records'][0]['s3']['object']['key']
     website_bucket = 'www.adriancaballeroresume.com'
     
     try:
-        #move s3 branch content to website s3
-        copy_source = {
-            'Bucket': bucket,
-            'Key': key
-        }
-        bucket = s3.Bucket(website_bucket)
-        bucket.copy(copy_source, key)
+        # Get a list of all objects in the source bucket
+        source_bucket = s3.Bucket(bucket)
+        for obj in source_bucket.objects.all():
+            # Copy each object to the destination bucket
+            copy_source = {
+                'Bucket': bucket,
+                'Key': obj.key
+            }
+            s3.meta.client.copy(copy_source, website_bucket, obj.key)
 
         #publish message to SNS 
         message = f"Updated website content"
         sns.publish(TopicArn=sns_topic_arn, Message=message, Subject="Updated Website contents")
     except Exception as e:
         print(e)
-        print(f'Error in websitesyncing')
+        print(f'Error in website syncing')
         raise e
               
