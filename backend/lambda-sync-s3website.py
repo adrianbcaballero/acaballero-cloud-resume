2007137219b51bcb0b64d2ff0ae0f0fcb6eea0a8
adrianbcaballero <abcaballero.py@gmail.com>
2024-03-10 22:27:10 -0700

    fixing list used as key

--- backend/lambda-sync-s3website.py
+++ backend/lambda-sync-s3website.py
@@ -7,15 +7,15 @@ sns = boto3.client('sns')
 
 def lambda_handler(event, context):
     try:
-        #delete objects in www.adriancaballeroresume.com
+        # Delete objects in www.adriancaballeroresume.com
         website_bucket_name = 'www.adriancaballeroresume.com'
         prefix = ''
         response = s3.list_objects_v2(Bucket=website_bucket_name, Prefix=prefix)
-        for object in response['Contents', []]:
+        for object in response.get('Contents', []):
             print('Deleting', object['Key'])
-            s3.delete_object(Bucket=website_bucket_name, Key=obj['Key'])
+            s3.delete_object(Bucket=website_bucket_name, Key=object['Key'])
         
-       # copying contents from branch content s3 to website s3
+        # Copying contents from branch content s3 to website s3
         src_bucket_name = 'adriancaballero-branchcontent'
         dest_bucket_name = 'www.adriancaballeroresume.com'
 
@@ -26,7 +26,7 @@ def lambda_handler(event, context):
             s3.copy_object(CopySource=copy_source, Bucket=dest_bucket_name, Key=obj['Key'])
             print(obj['Key'] + ' - File Copied')
 
-        #send sns of website updating
+        # Send SNS of website updating
         sns_topic_arn = os.environ['SNS_TOPIC_ARN']
         message = "New front end files were uploaded and Website content will be updated"
         sns.publish(TopicArn=sns_topic_arn, Message=message, Subject="Updated Website contents")
@@ -42,4 +42,3 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps('Error occurred while updating website content')
         }
-              
