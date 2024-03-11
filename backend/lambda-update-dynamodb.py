c5e9a527d42b707f8c717803ad52c0044d10de1e
adrianbcaballero <abcaballero.py@gmail.com>
2024-03-11 16:42:33 -0700

    lambda to update item in dynamoDB

--- backend/lambda-update-dynamodb.py
+++ backend/lambda-update-dynamodb.py
@@ -1,4 +1,21 @@
 import boto3
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodb = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-    print("Success")
+    try:
+        response = dynamodb.update_item(
+        TableName = "Website-access"
+        Key={"website_id": {"S": "hey"}},
+        UpdateExpression="set access_count = if_not_exists(access_count, :start) + :increase",
+        ExpressionAttributeValues={":start": {"N": "0"}, ":increase": {"N": "1"}},
+        ReturnValues="UPDATED_NEW",
+            )
+
+        logger.info("Response: %s", response)
+    except Exception as e:
+        logger.error("An error occurred: %s", str(e))
