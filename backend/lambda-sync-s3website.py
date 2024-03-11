e54b7bd40a7f57de950a963e604b21813d490e39
adrianbcaballero <abcaballero.py@gmail.com>
2024-03-10 18:36:46 -0700

    allows for s3 object deletion in lambda

--- backend/lambda-sync-s3website.py
+++ backend/lambda-sync-s3website.py
@@ -6,15 +6,28 @@ s3 = boto3.client('s3')
 sns = boto3.client('sns')
 
 def lambda_handler(event, context):
-    # Get the object from the event and show its content type
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = event['Records'][0]['s3']['object']['key']
-    sns_topic_arn = os.environ['SNS_TOPIC_ARN']
     try:
+        #delete objects in www.adriancaballeroresume.com
+        website_bucket_name = 'www.adriancaballeroresume.com'
+        prefix = ''
+        response = s3_client.list_objects_v2(Bucket=website_bucket_name, Prefix=prefix)
+        for object in response['Contents', []]:
+            print('Deleting', object['Key'])
+            s3_client.delete_object(Bucket=website_bucket_name, Key=object['Key'])
+        
+        #send sns of website updating
+        sns_topic_arn = os.environ['SNS_TOPIC_ARN']
         message = "New front end files were uploaded and Website content will be updated"
         sns.publish(TopicArn=sns_topic_arn, Message=message, Subject="Updated Website contents")
         
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Website content updated and SNS sent successfully')
+        }
     except Exception as e:
-        print("Error publishing message to SNS topic:", e)
-        raise e
+        print("An error occurred:", str(e))
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error occurred while updating website content')
+        }
               
