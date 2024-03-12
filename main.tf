588cb46b4398f707d037896821209504ea35ec78
adrianbcaballero <abcaballero.py@gmail.com>
2024-03-11 22:06:10 -0700

    update lambda and add starting count

--- main.tf
+++ main.tf
@@ -173,6 +173,17 @@ resource "aws_dynamodb_table" "website-dynamodb-table" {
   }
 }
 
+//initialize at zero
+resource "aws_dynamodb_table_item" "initial_website_item" {
+  table_name = aws_dynamodb_table.website-dynamodb-table.name
+
+  hash_key = "website_id"
+  item = {
+    website_id   = "adriancaballeroresume.com"
+    access_count = 0
+  }
+}
+
 //lambda function triggered by api gateway
 data "archive_file" "lambda-update-dynamodb" {
   type = "zip"
