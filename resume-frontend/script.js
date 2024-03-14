0ccd89ec4e415e9cd07f69f1224162fe91e65c2c
adrianbcaballero <abcaballero.py@gmail.com>
2024-03-13 23:11:04 -0700

    new error page and fixed reset of view count

--- resume-frontend/script.js
+++ resume-frontend/script.js
@@ -11,8 +11,9 @@ fetch('https://r05eb4w5b0.execute-api.us-west-1.amazonaws.com/dev/proxy', {
     return response.json(); 
 })
 .then(data => {
-    console.log('Response data', data);
-    const newValue = data.value;
+    console.log('Response data:', data); 
+    const parsedBody = JSON.parse(data.body); 
+    const newValue = parsedBody.value; 
 
     document.getElementById('viewCount').innerText = `View Count: ${newValue}`;
 })
